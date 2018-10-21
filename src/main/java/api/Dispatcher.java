feb5a7b5feac8f0ce67868c7c2a571b715e7e454
Xavier <x.sotomayor@alumnos.upm.es>
2018-10-21 12:48:17 +0200

    Add Dispatcher

---
+++ src/main/java/api/Dispatcher.java
@@ -0,0 +1,64 @@
+package api;
+
+import api.exceptions.ArgumentNotValidException;
+import api.exceptions.NotFoundException;
+import api.exceptions.RequestInvalidException;
+import http.HttpRequest;
+import http.HttpResponse;
+import http.HttpStatus;
+
+public class Dispatcher {
+
+
+    public void submit(HttpRequest request, HttpResponse response) {
+        String ERROR_MESSAGE = "{'error':'%S'}";
+        try {
+            switch (request.getMethod()) {
+                case POST:
+                    this.doPost(request, response);
+                    break;
+                case GET:
+                    this.doGet(request, response);
+                    break;
+                case PUT:
+                    this.doPut(request);
+                    break;
+                case PATCH:
+                    this.doPatch(request);
+                    break;
+                case DELETE:
+                    this.doDelete(request);
+                    break;
+                default: // Unexpected
+                    throw new RequestInvalidException("method error: " + request.getMethod());
+            }
+        } catch (ArgumentNotValidException | RequestInvalidException exception) {
+            response.setBody(String.format(ERROR_MESSAGE, exception.getMessage()));
+            response.setStatus(HttpStatus.BAD_REQUEST);
+        } catch (NotFoundException exception) {
+            response.setBody(String.format(ERROR_MESSAGE, exception.getMessage()));
+            response.setStatus(HttpStatus.NOT_FOUND);
+        } catch (Exception exception) {  // Unexpected
+            exception.printStackTrace();
+            response.setBody(String.format(ERROR_MESSAGE, exception));
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void doDelete(HttpRequest request) {
+    }
+
+    private void doPatch(HttpRequest request) {
+    }
+
+    private void doPut(HttpRequest request) {
+    }
+
+    private void doGet(HttpRequest request, HttpResponse response) {
+    }
+
+    private void doPost(HttpRequest request, HttpResponse response) {
+    }
+
+
+}
